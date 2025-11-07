99123c962f29f8dc9a2c557bba1f09de3514b060
jskwang84-sudo <jskwang84@gmail.com>
2025-11-07 23:23:05 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,44 @@
 const CACHE_NAME = 'nh-bank-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-// 설치 이벤트
-self.addEventListener('install', event => {
+// 설치
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
-// 활성화 이벤트
-self.addEventListener('activate', event => {
+// 활성화
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k))))
+    )
   );
+  self.clients.claim();
 });
 
-// Fetch 이벤트
-self.addEventListener('fetch', event => {
+// 네트워크 우선 + 캐시 폴백
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+    fetch(req)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((c) => c.put(req, resClone));
+        return res;
       })
+      .catch(() =>
+        caches.match(req).then((cached) => cached || caches.match('./'))
+      )
   );
 });
